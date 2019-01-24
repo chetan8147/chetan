2c0d6b48459e416264d17a0a31d12b6308783238
chetan8147 <46998300+chetan8147@users.noreply.github.com>
2019-01-24 18:39:38 +0530

    push from chetan

--- src/demooo/Demoaa.java
+++ src/demooo/Demoaa.java
@@ -5,6 +5,12 @@ public class Demoaa {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("SSSsddfdf");
+System.out.println("hsgfsjddgfsdj");
+	}
+	
+	public static void xyz()
+	{
+		System.out.println("sdasvvasd");
 	}
 
 }
